d0239094ad9e2cf8b4e734c894328c90cee939c2
chrisinko <128570604+chrisinko@users.noreply.github.com>
2023-03-22 17:50:11 +0900

    Add files via upload

---
+++ week2/SecondWeekAssignment.java
@@ -0,0 +1,21 @@
+package Hello;
+
+import java.util.Scanner;
+
+public class SecondWeekAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		float a,b,c;
+		a = sc.nextFloat();
+		b = sc.nextFloat();
+		c = sc.nextFloat();
+		System.out.println(a+b+c);
+		
+		
+	}
+
+}
