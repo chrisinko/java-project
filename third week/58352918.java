bbfe65f5134dec953104d41111e4d69d22bb622c
chrisinko <128570604+chrisinko@users.noreply.github.com>
2023-03-29 17:00:19 +0900

    Add files via upload

---
+++ third week/58352918.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODdO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		System.out.println(a+"??!");
+	
+		
+		
+		
+		
+	}
+
+}
